5165e882eef9bd88a2c26d8ea1e69a033d244dd3
ZhangJun-ZhangJun_PC <sn2015.zhang@gmail.com>
2017-12-24 18:50:47 +0800

    Initial commit,now it can get exam data.

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import flex.messaging.io.amf.client.AMFConnection;
+import flex.messaging.io.amf.client.exceptions.ClientStatusException;
+import flex.messaging.io.amf.client.exceptions.ServerStatusException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("AmfParser Running!");
+        System.out.println("By ZhangJun");
+        System.out.println("Development Version-0.125.1");
+        AMFConnection amfConnection = new AMFConnection();
+        try{
+            String url="http://211.141.133.22:8081/SchoolCenter/messagebroker/amf";
+            amfConnection.connect(url);
+            Object result = amfConnection.call("multiExamServiceNew.getAllStudentMultiExam",19600,"0120151513","HDC/gq15+GtcBjjeW6hTYw==");
+
+            System.out.println(result.toString());
+        } catch (ClientStatusException e) {
+            e.printStackTrace();
+        } catch (ServerStatusException e) {
+            e.printStackTrace();
+        } finally {
+            amfConnection.close();
+        }
+    }
+}
