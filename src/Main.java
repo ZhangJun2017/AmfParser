7ff276b945a7478a9bf09ea93500954f0f05d1ac
ZGZX <616388258@qq.com>
2017-12-26 12:38:58 +0800

    now it can handle null data.

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("AmfParser Running!");
         System.out.println("By ZhangJun");
-        System.out.println("Development Version-0.125.2");
+        System.out.println("Development Version-0.125.3");
         AMFConnection amfConnection = new AMFConnection();
         try{
             String url="http://211.141.133.22:8081/SchoolCenter/messagebroker/amf";
