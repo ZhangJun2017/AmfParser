07bf3e1ba5f17255ed0a6debcd7a7e341d773cca
ZGZX <616388258@qq.com>
2017-12-26 12:34:39 +0800

    now it can handle null data.

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,20 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("AmfParser Running!");
         System.out.println("By ZhangJun");
-        System.out.println("Development Version-0.125.1");
+        System.out.println("Development Version-0.125.2");
         AMFConnection amfConnection = new AMFConnection();
         try{
             String url="http://211.141.133.22:8081/SchoolCenter/messagebroker/amf";
             amfConnection.connect(url);
             Object result = amfConnection.call("multiExamServiceNew.getAllStudentMultiExam",19600,"0120151513","HDC/gq15+GtcBjjeW6hTYw==");
+            String resultString = result.toString();
+            if (resultString == "[]") {
+                System.out.println("数据错误");
+            } else {
+                //System.out.println(result.equals(result));
+                System.out.println(resultString);
 
-            System.out.println(result.toString());
+            }
         } catch (ClientStatusException e) {
             e.printStackTrace();
         } catch (ServerStatusException e) {
