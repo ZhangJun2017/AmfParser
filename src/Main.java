fb093fb236cb0b383b1863f3415612349122a9ec
ZhangJun-ZhangJun_PC <sn2015.zhang@gmail.com>
2017-12-31 21:14:11 +0800

    2017 last update,add doc for preventing I forget how to make api.

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("AmfParser Running!");
         System.out.println("By ZhangJun");
-        System.out.println("Development Version-0.2.4");
+        System.out.println("Development Version-0.2.5");
         AMFConnection amfConnection = new AMFConnection();
         try{
             String url="http://211.141.133.22:8081/SchoolCenter/messagebroker/amf";
             amfConnection.connect(url);
             Object result = amfConnection.call("multiExamServiceNew.getAllStudentMultiExam", 19600, "0120151513", "");
+            //拿数据
             System.out.println("解析数据：");
             System.out.println("====================");
             System.out.println(result);
@@ -41,27 +42,34 @@ public class Main {
             //System.out.println(asObject);
             java.util.ArrayList rootMap = (java.util.ArrayList) asObject.get("source");
             //从有3个[]的body里开始解析
+            //[MARK:1]
             //System.out.println(tree);
             System.out.println("解析到的数据map为：");
             System.out.println(rootMap);
             ASObject examMap = (ASObject) rootMap.get(0);
+            //[MARK:2]
             //[0]永远是最新一次考试
             System.out.println("解析到的数据exam_map为：");
             System.out.println(examMap);
             Object id = examMap.get("studentId");
+            //[MARK:3]
             System.out.println("解析到的学号为：");
             System.out.println(id);
             ASObject scoreMap = (ASObject) examMap.get("seStudentScoreList");
+            //[MARK:4]
             System.out.println("解析到的各科数据为：");
             System.out.println(scoreMap);
             java.util.ArrayList eachTypeRoot = (java.util.ArrayList) scoreMap.get("source");
             //root类map一定要用array list解析
+            //[MARK:5]
             System.out.println("解析到的各科root数据为：");
             System.out.println(eachTypeRoot);
             ASObject eachType1 = (ASObject) eachTypeRoot.get(0);
+            //[MARK:6]
             System.out.println("解析到的第一科数据为：");
             System.out.println(eachType1);
             Object school = eachType1.get("schoolName");
+            //[MARK:7]
             //普通的值用object，print时直接print这个object就行了，文件夹要用asobject，还要用(asobject)强制转换。
             System.out.println("解析到的学校数据为：");
             System.out.println(school);
