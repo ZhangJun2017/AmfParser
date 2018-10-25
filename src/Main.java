3a81201da466c3664beeb418f7cc0e3ca1f3bbd3
ZhangJun-GZZX_PC <sn2015.zhang@gmail.com>
2018-10-25 11:59:21 +0800

    v0.5:Console Friendly Update is Coming!

--- src/Main.java
+++ src/Main.java
@@ -5,42 +5,53 @@ import com.weedong.flex.messaging.io.ASObject;
 import utils.Tools;
 import utils.values;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
         values values = new values();
+        Scanner scanner = new Scanner(System.in);
+        boolean whileQuery = true;
+        if (args.length != 0) {
+            whileQuery = false;
+            values.studentID = args[0];
+        } else {
+            System.out.print("Waiting for input... > ");
+            values.studentID = scanner.next();
+        }
         Tools tools = new Tools();
         System.out.println("AmfParser Running!");
         System.out.println("By ZhangJun");
         System.out.println("Development Version-" + values.version);
-        if (args.length == 0) {
-            System.out.println("没有指定学号，使用默认学号。");
-        } else {
-            values.studentID = args[0];
-        }
-        AMFConnection amfConnection = new AMFConnection();
-        try {
-            amfConnection.connect(values.url);
-            Object result = amfConnection.call(values.command, 19868, values.studentID, "");
-            //拿数据
-            System.out.println("解析数据：");
-            System.out.println("====================");
-            System.out.println(result);
-            System.out.println("====================");
-            ASObject asObject = (ASObject) result;
-            java.util.ArrayList rootMap = (java.util.ArrayList) asObject.get("source");
-            System.out.println("解析到的数据map为：");
-            System.out.println(rootMap);
-            System.out.println("====================");
-            tools.query(rootMap, asObject);
-        } catch (ClientStatusException e) {
-            e.printStackTrace();
-        } catch (ServerStatusException e) {
-            e.printStackTrace();
-        } finally {
-            amfConnection.close();
-        }
+        do {
+            AMFConnection amfConnection = new AMFConnection();
+            try {
+                amfConnection.connect(values.url);
+                Object result = amfConnection.call(values.command, 19868, values.studentID, "");
+                //拿数据
+                System.out.println("解析数据：");
+                System.out.println("====================");
+                System.out.println(result);
+                System.out.println("====================");
+                ASObject asObject = (ASObject) result;
+                java.util.ArrayList rootMap = (java.util.ArrayList) asObject.get("source");
+                System.out.println("解析到的数据map为：");
+                System.out.println(rootMap);
+                System.out.println("====================");
+                tools.query(rootMap, asObject);
+                if (whileQuery == true) {
+                    System.out.println();
+                    System.out.print("Waiting for input... > ");
+                    values.studentID = scanner.next();
+                }
+            } catch (ClientStatusException e) {
+                e.printStackTrace();
+            } catch (ServerStatusException e) {
+                e.printStackTrace();
+            } finally {
+                amfConnection.close();
+            }
+        } while (whileQuery);
     }
-
 }
